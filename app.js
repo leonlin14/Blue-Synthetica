bfe66f7f736dee7aa2e2a5d115bf05f166457e27
leonlin <leonlin14@gmail.com>
2016-09-28 11:10:55 +0800

    create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+var koa = require('koa');
+var path = require('path');
+var serve = require('koa-static');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = koa();
+
+// serve the files out of ./public as our main files
+app.use(serve(path.join(__dirname, 'public')));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
